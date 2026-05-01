d026652e73f51a1825e7691d168d6ba55ec95155
Ahmed Shalash <ahmed02shalash@gmail.com>
2026-05-01 06:08:27 +0300

    added a code to read the encoder position

---
+++ Codes/read_encoder_position/src/main.cpp
@@ -0,0 +1,32 @@
+#include <Arduino.h>
+
+// Encoder connections
+const int encoderPinA = 2;  // Channel A
+const int encoderPinB = 3;  // Channel B
+
+volatile long pos = 0;
+void readEncoder();
+
+void setup() {
+  Serial.begin(115200);
+
+  pinMode(encoderPinA, INPUT_PULLUP);
+  pinMode(encoderPinB, INPUT_PULLUP);
+
+  attachInterrupt(digitalPinToInterrupt(encoderPinA), readEncoder, RISING);
+
+}
+
+void loop() {
+  Serial.println(pos);
+}
+
+void readEncoder() {
+  int A = digitalRead(encoderPinA);
+  int B = digitalRead(encoderPinB);
+  if (digitalRead(encoderPinB))
+    pos++;
+  else
+    pos--;
+
+}
