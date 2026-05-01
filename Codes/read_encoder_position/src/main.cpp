ad8d0e381a70af442fdeddd61d77fefcff9b9c7a
Ahmed Shalash <ahmed02shalash@gmail.com>
2026-05-01 08:19:57 +0300

    added a code to read the RPM

--- Codes/read_encoder_position/src/main.cpp
+++ Codes/read_encoder_position/src/main.cpp
@@ -4,6 +4,9 @@
 const int encoderPinA = 2;  // Channel A
 const int encoderPinB = 3;  // Channel B
 
+const float PPR = 514.8;  // as the gear ratio of the motor is 1:46.8, each encoder channel produces 11 signals per revolution, so PPR = 11 * 46.8 = 514.8
+unsigned long old_time = 0;
+
 volatile long pos = 0;
 void readEncoder();
 
@@ -18,11 +21,18 @@ void setup() {
 }
 
 void loop() {
-  //  atomic read
-  noInterrupts();
-  long current_position = pos;
-  interrupts();
-  Serial.println(current_position);
+  unsigned long current_time = millis();
+
+  if (current_time - old_time >= 1000) {
+    noInterrupts();
+    long count = pos;
+    pos = 0;
+    interrupts();
+
+    float RPM = (count * 60000.0) / (PPR * (current_time - old_time));
+    old_time = current_time;
+    Serial.println(RPM);
+  }
 }
 
 void readEncoder() {
@@ -30,5 +40,4 @@ void readEncoder() {
     pos++;
   else
     pos--;
-
 }
